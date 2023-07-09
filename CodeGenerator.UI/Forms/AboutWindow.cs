633e7ec53d4f4437b0091e65cb48f4c1aec9976f
Matt <gordon-matt@hotmail.com>
2023-07-09 15:15:06 +0930

    Fixed links in "About Window", removed unused NuGet packages and updated a couple of others.

--- CodeGenerator.UI/Forms/AboutWindow.cs
+++ CodeGenerator.UI/Forms/AboutWindow.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace CodeGenerator.UI;
 
@@ -7,13 +8,13 @@ namespace CodeGenerator.UI;
 /// </summary>
 public class AboutWindow : KryptonForm
 {
-    private Label lblYear;
-    private LinkLabel lnkUrl;
-    private Label lblHeading;
     private Label lblAuthor;
+    private Label lblContributor;
+    private Label lblHeading;
+    private Label lblYear;
     private LinkLabel lnkAuthor;
     private LinkLabel lnkContributor;
-    private Label lblContributor;
+    private LinkLabel lnkUrl;
 
     public AboutWindow()
     {
@@ -30,132 +31,152 @@ public class AboutWindow : KryptonForm
     /// </summary>
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutWindow));
-        this.lblHeading = new System.Windows.Forms.Label();
-        this.lblYear = new System.Windows.Forms.Label();
-        this.lnkUrl = new System.Windows.Forms.LinkLabel();
-        this.lblAuthor = new System.Windows.Forms.Label();
-        this.lnkAuthor = new System.Windows.Forms.LinkLabel();
-        this.lnkContributor = new System.Windows.Forms.LinkLabel();
-        this.lblContributor = new System.Windows.Forms.Label();
-        this.SuspendLayout();
+        var resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutWindow));
+        lblHeading = new Label();
+        lblYear = new Label();
+        lnkUrl = new LinkLabel();
+        lblAuthor = new Label();
+        lnkAuthor = new LinkLabel();
+        lnkContributor = new LinkLabel();
+        lblContributor = new Label();
+        SuspendLayout();
         //
         // lblHeading
         //
-        this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-        this.lblHeading.Location = new System.Drawing.Point(14, 10);
-        this.lblHeading.Name = "lblHeading";
-        this.lblHeading.Size = new System.Drawing.Size(322, 28);
-        this.lblHeading.TabIndex = 0;
-        this.lblHeading.Text = "Code Generator";
-        this.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+        lblHeading.Location = new System.Drawing.Point(14, 10);
+        lblHeading.Name = "lblHeading";
+        lblHeading.Size = new System.Drawing.Size(322, 28);
+        lblHeading.TabIndex = 0;
+        lblHeading.Text = "Code Generator";
+        lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         //
         // lblYear
         //
-        this.lblYear.Location = new System.Drawing.Point(18, 38);
-        this.lblYear.Name = "lblYear";
-        this.lblYear.Size = new System.Drawing.Size(318, 28);
-        this.lblYear.TabIndex = 2;
-        this.lblYear.Text = "2005 - ";
-        this.lblYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lblYear.Location = new System.Drawing.Point(18, 38);
+        lblYear.Name = "lblYear";
+        lblYear.Size = new System.Drawing.Size(318, 28);
+        lblYear.TabIndex = 2;
+        lblYear.Text = "2005 - ";
+        lblYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         //
         // lnkUrl
         //
-        this.lnkUrl.Location = new System.Drawing.Point(18, 66);
-        this.lnkUrl.Name = "lnkUrl";
-        this.lnkUrl.Size = new System.Drawing.Size(318, 29);
-        this.lnkUrl.TabIndex = 3;
-        this.lnkUrl.TabStop = true;
-        this.lnkUrl.Text = "http://www.icodegenerator.net";
-        this.lnkUrl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        this.lnkUrl.VisitedLinkColor = System.Drawing.Color.Red;
-        this.lnkUrl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkUrl_LinkClicked);
+        lnkUrl.Location = new System.Drawing.Point(18, 66);
+        lnkUrl.Name = "lnkUrl";
+        lnkUrl.Size = new System.Drawing.Size(318, 29);
+        lnkUrl.TabIndex = 3;
+        lnkUrl.TabStop = true;
+        lnkUrl.Text = "http://www.icodegenerator.net";
+        lnkUrl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lnkUrl.VisitedLinkColor = System.Drawing.Color.Red;
+        lnkUrl.LinkClicked += lnkUrl_LinkClicked;
         //
         // lblAuthor
         //
-        this.lblAuthor.Location = new System.Drawing.Point(18, 111);
-        this.lblAuthor.Name = "lblAuthor";
-        this.lblAuthor.Size = new System.Drawing.Size(318, 28);
-        this.lblAuthor.TabIndex = 1;
-        this.lblAuthor.Text = "Original Author: Victor Y. Dominguez";
-        this.lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lblAuthor.Location = new System.Drawing.Point(18, 111);
+        lblAuthor.Name = "lblAuthor";
+        lblAuthor.Size = new System.Drawing.Size(318, 28);
+        lblAuthor.TabIndex = 1;
+        lblAuthor.Text = "Original Author: Victor Y. Dominguez";
+        lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         //
         // lnkAuthor
         //
-        this.lnkAuthor.Location = new System.Drawing.Point(18, 139);
-        this.lnkAuthor.Name = "lnkAuthor";
-        this.lnkAuthor.Size = new System.Drawing.Size(318, 28);
-        this.lnkAuthor.TabIndex = 4;
-        this.lnkAuthor.TabStop = true;
-        this.lnkAuthor.Text = "http://www.vientodigital.com/";
-        this.lnkAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        this.lnkAuthor.VisitedLinkColor = System.Drawing.Color.Red;
+        lnkAuthor.Location = new System.Drawing.Point(18, 139);
+        lnkAuthor.Name = "lnkAuthor";
+        lnkAuthor.Size = new System.Drawing.Size(318, 28);
+        lnkAuthor.TabIndex = 4;
+        lnkAuthor.TabStop = true;
+        lnkAuthor.Text = "http://www.vientodigital.com/";
+        lnkAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lnkAuthor.VisitedLinkColor = System.Drawing.Color.Red;
+        lnkAuthor.LinkClicked += lnkAuthor_LinkClicked;
         //
         // lnkContributor
         //
-        this.lnkContributor.Location = new System.Drawing.Point(18, 196);
-        this.lnkContributor.Name = "lnkContributor";
-        this.lnkContributor.Size = new System.Drawing.Size(318, 28);
-        this.lnkContributor.TabIndex = 6;
-        this.lnkContributor.TabStop = true;
-        this.lnkContributor.Text = "https://github.com/gordon-matt";
-        this.lnkContributor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        this.lnkContributor.VisitedLinkColor = System.Drawing.Color.Red;
+        lnkContributor.Location = new System.Drawing.Point(18, 196);
+        lnkContributor.Name = "lnkContributor";
+        lnkContributor.Size = new System.Drawing.Size(318, 28);
+        lnkContributor.TabIndex = 6;
+        lnkContributor.TabStop = true;
+        lnkContributor.Text = "https://github.com/gordon-matt";
+        lnkContributor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lnkContributor.VisitedLinkColor = System.Drawing.Color.Red;
+        lnkContributor.LinkClicked += lnkContributor_LinkClicked;
         //
         // lblContributor
         //
-        this.lblContributor.Location = new System.Drawing.Point(18, 167);
-        this.lblContributor.Name = "lblContributor";
-        this.lblContributor.Size = new System.Drawing.Size(318, 29);
-        this.lblContributor.TabIndex = 5;
-        this.lblContributor.Text = "Contributor: Matt Gordon";
-        this.lblContributor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        lblContributor.Location = new System.Drawing.Point(18, 167);
+        lblContributor.Name = "lblContributor";
+        lblContributor.Size = new System.Drawing.Size(318, 29);
+        lblContributor.TabIndex = 5;
+        lblContributor.Text = "Contributor: Matt Gordon";
+        lblContributor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
         //
         // AboutWindow
         //
-        this.AutoScaleBaseSize = new System.Drawing.Size(6, 16);
-        this.BackColor = System.Drawing.SystemColors.Window;
-        this.ClientSize = new System.Drawing.Size(364, 241);
-        this.Controls.Add(this.lnkContributor);
-        this.Controls.Add(this.lblContributor);
-        this.Controls.Add(this.lnkAuthor);
-        this.Controls.Add(this.lnkUrl);
-        this.Controls.Add(this.lblYear);
-        this.Controls.Add(this.lblAuthor);
-        this.Controls.Add(this.lblHeading);
-        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-        this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-        this.Name = "AboutWindow";
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        this.Text = "CodeGenerator";
-        this.Click += new System.EventHandler(this.AboutWindow_Click);
-        this.ResumeLayout(false);
+        AutoScaleBaseSize = new System.Drawing.Size(6, 16);
+        BackColor = System.Drawing.SystemColors.Window;
+        ClientSize = new System.Drawing.Size(364, 241);
+        Controls.Add(lnkContributor);
+        Controls.Add(lblContributor);
+        Controls.Add(lnkAuthor);
+        Controls.Add(lnkUrl);
+        Controls.Add(lblYear);
+        Controls.Add(lblAuthor);
+        Controls.Add(lblHeading);
+        FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+        Name = "AboutWindow";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "CodeGenerator";
+        Click += AboutWindow_Click;
+        ResumeLayout(false);
     }
 
     #endregion Windows Form Designer generated code
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable for WinForms event handlers")]
-    private void lnkUrl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    private static void VisitLink(LinkLabel linkLabel)
     {
+        linkLabel.LinkVisited = true;
+        string url = linkLabel.Text;
+
         try
         {
-            VisitLink();
+            using var process = Process.Start(url);
         }
         catch
         {
-            MessageBox.Show("Unable to open link that was clicked.");
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                using var process = Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                using var process = Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                using var process = Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
         }
     }
 
-    private void VisitLink()
-    {
-        // Change the color of the link text by setting LinkVisited
-        // to true.
-        lnkUrl.LinkVisited = true;
-        //Call the Process.Start method to open the default browser
-        //with a URL:
-        _ = Process.Start(lnkUrl.Text);
-    }
-
     private void AboutWindow_Click(object sender, EventArgs e) => Hide();
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable for WinForms event handlers")]
+    private void lnkAuthor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) => VisitLink(lnkAuthor);
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable for WinForms event handlers")]
+    private void lnkContributor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) => VisitLink(lnkContributor);
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Acceptable for WinForms event handlers")]
+    private void lnkUrl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) => VisitLink(lnkUrl);
 }
